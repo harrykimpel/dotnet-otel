898c35bc56bcd3d6f28a70bea3f6b5d18359e45d
Harry Kimpel <harrykimpel@hotmail.com>
2023-03-23 17:30:35 +0100

    Adding OpenTelemetry Logs to the service

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Metrics;
 using System.Diagnostics.Metrics;
-using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Add OpenTelemetry to our Service Collection
+// Add OpenTelemetry Traces and Metrics to our Service Collection
 builder.Services.AddOpenTelemetry()
     .WithTracing(tracerProviderBuilder =>
         tracerProviderBuilder
@@ -20,11 +20,7 @@ builder.Services.AddOpenTelemetry()
             .AddAspNetCoreInstrumentation()
             .AddConsoleExporter()
             .AddOtlpExporter()
-        );
-
-// Add metrics to the app
-builder.Services.AddOpenTelemetry()
-    .WithTracing(/*  .. tracing setup */ )
+        )
     .WithMetrics(metricsProviderBuilder =>
         metricsProviderBuilder
             .ConfigureResource(resource => resource
@@ -34,6 +30,16 @@ builder.Services.AddOpenTelemetry()
             .AddMeter(DiagnosticsConfig.Meter.Name)
             .AddOtlpExporter()
         );
+
+// Add OpenTelemetry Logs to our Service Collection
+builder.Logging.AddOpenTelemetry(x =>
+{
+    x.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MyService"));
+    x.IncludeFormattedMessage = true;
+    x.IncludeScopes = true;
+    x.ParseStateValues = true;
+    x.AddOtlpExporter();
+});
 
 var app = builder.Build();
 
